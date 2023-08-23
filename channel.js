6d82a9b4849b4e0d339a37c13dfb8b8c1908a269
JBJ <maystar5987@gmail.com>
2023-08-23 17:50:29 +0900

    중간점검

---
+++ channel.js
@@ -0,0 +1,57 @@
+function formatDate(date) {
+  const today = new Date();
+  const targetDate = new Date(date);
+
+  if (
+    targetDate.getFullYear() === today.getFullYear() &&
+    targetDate.getMonth() === today.getMonth() &&
+    targetDate.getDate() === today.getDate()
+  ) {
+    return targetDate.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+    });
+  } else {
+    const year = targetDate.getFullYear();
+    const month = String(targetDate.getMonth() + 1).padStart(2, '0');
+    const day = String(targetDate.getDate()).padStart(2, '0');
+    return `${year}.${month}.${day}`;
+  }
+}
+
+function tableTemplate(item) {
+  const formattedDate = formatDate(item.createdTime);
+
+  let truncatedTitle = item.title;
+  if (truncatedTitle.length > 20) {
+    truncatedTitle = truncatedTitle.substring(0, 17) + '...';
+  }
+
+  return /*html*/ `
+  <tr data-no="${item.no}">
+  <td>${item.no}</td>
+  <td><a href="http://localhost:5500/post-view/${item.no}">${truncatedTitle}</a></td>
+  <td>익명</td>
+  <td>${formattedDate}</td>
+  </tr>
+  `;  
+}
+
+(async()=>{
+  const url = "http://localhost:8080/posts";
+
+    const response = await fetch(url);
+    const result = await response.json();
+    const data = Array.from(result);
+
+    const table = document.querySelector(".post-table");
+    const tbody = table.querySelector("tbody");
+
+      // 최신순 정렬
+    data
+      .sort((a, b) => a.no - b.no)
+      .forEach((item) => {
+        tbody.insertAdjacentHTML("afterend", tableTemplate(item));
+      });
+})();
--- post-create.js
+++ post-create.js
@@ -17,39 +17,8 @@ function cardTemplate(item) {
   return template;
 }
 
-// 채널 테이블용
-function tableTemplate(item) {
-  const template = /*html*/ `
-  <tr data-no="${item.no}">
-  <td>${item.no}</td>
-  <td>${item.title}</td>
-  <td>${item.creatorName}</td>
-  <td>${new Date(item.createdTime).toLocaleString()}</td>
-  </tr>
-  `
-}
-
-(async () => {
-  const url = "http://localhost:8080/posts";
-
-  const response = await fetch(url);
-  const result = await response.json();
-
-  const data = Array.from(result);
-
-  // 최신순 정렬
-  // data
-  //   .sort((a, b) => a.no - b.no)
-  //   .forEach((item) => {
-  //     document.forms[2].insertAdjacentHTML(
-  //       "afterend", cardTemplate(item)
-  //     );
-  //   });
-})();
-
 // 뷰 페이지 추가
 (() => {
-  const form = document.querySelector("post-form");
   const post = document.getElementsByClassName("post-button");
 
   post[0].addEventListener("click", async (e) => {
@@ -58,27 +27,34 @@ function tableTemplate(item) {
     const title = document.getElementsByClassName("input-title");
     const content = document.getElementsByClassName("input-content");
 
-    console.log(title[0].value);
-    console.log(content[0].value);
-
     const titleVal = title[0].value;
-    const contentVal = content[0].vlaue;
+    const contentVal = content[0].value;
 
-    const response = await fetch(
-      "http://localhost:8080/posts",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: titleVal,
-          content: contentVal,
-        }),
-      }
-    );
+    console.log(titleVal);
+    console.log(contentVal);
+    
+    const confirmVal = confirm("작성 하시겠습니까?");
+    if(confirmVal) {
+      const response = await fetch(
+        "http://localhost:8080/posts",  
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: titleVal,
+            content: contentVal,
+          }),
+        }
+      );
+  
+      const result = await response.json();
+      console.log(result);
+      
+      alert("작성 완료되었습니다!");
+      window.location.href = "http://127.0.0.1:5500/Channel.html";
 
-    const result = await response.json();
-    console.log(result);
+    } else return null;
   });
 })();
